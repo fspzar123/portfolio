ae9fd5d24826a6fe60ec9a6d12cb235846ac28cb
fspzar123 <fspzar123@gmail.com>
2024-09-17 13:00:20 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,25 @@ import logo from "../assets/kevinRushLogo.png"
 import { FaLinkedin} from "react-icons/fa6";
 import { FaGithub} from "react-icons/fa6";
 import { FaSquareXTwitter} from "react-icons/fa6";
-import { FaInstagram} from "react-icons/fa6";
+import { FaDiscord} from "react-icons/fa6";
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
         <img className = "mx-2 w-10" src = {logo} alt="logo"/>
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+        <a href="https://www.linkedin.com/in/filbert-shawn/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/fspzar123" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://discord.com/fspzar123" target="_blank" rel="noopener noreferrer">
+          <FaDiscord />
+        </a>
+        <a href="https://x.com/shawn23_may" target="_blank" rel="noopener noreferrer">
+          <FaSquareXTwitter />
+        </a>
     </div>
         
   </nav>
